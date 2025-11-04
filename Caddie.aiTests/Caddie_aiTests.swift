32dc91eba5ffd999283ee5a3b8c79b22be30aab4
Joe Tashjy <joe.tashjy@gmail.com>
2025-11-04 10:22:01 -0500

    Initial Commit

---
+++ Caddie.aiTests/Caddie_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  Caddie_aiTests.swift
+//  Caddie.aiTests
+//
+//  Created by Joe Tashjy on 11/4/25.
+//
+
+import Testing
+@testable import Caddie_ai
+
+struct Caddie_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
